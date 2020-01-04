4cc5ddf55e8de9d023b503963a67939c6d39ce6f
Jose Andres Villa Loza <pepeandrew999@gmail.com>
2020-01-03 22:35:10 -0400

    Primer commit - rest server

--- index.ts
+++ index.ts
@@ -1,2 +1,18 @@
-const nombre = 'Alberto';
-console.log(`Mi nombre es ${ nombre }`);
+import Server from './classes/server';
+import { SERVER_PORT } from './global/environment';
+import  router  from './routes/router';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const server = new Server();
+//BodyParser
+server.app.use(bodyParser.urlencoded({ extended:true }));
+server.app.use(bodyParser.json());
+//CORS
+server.app.use(cors({origin:true,credentials:true}));
+
+//Rutas de servicios
+server.app.use('/',router);
+server.start(()=>{
+    console.log(`Servidor corriendo en el puerto ${server.port}`);
+});
